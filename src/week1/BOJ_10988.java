874024c2ecccb63f036aded894de8f6c64fc7018
sojeonglee <sojeonglee0125@gmail.com>
2022-11-02 07:56:17 +0900

    solved : BOJ_10988

---
+++ src/week1/BOJ_10988.java
@@ -0,0 +1,13 @@
+package week1;
+
+import java.util.Scanner;
+
+public class BOJ_10988 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) System.out.println(1);
+        else System.out.println(0);
+    }
+}
