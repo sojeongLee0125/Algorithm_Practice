0c777a4965b7ee3289130afd0948de66807ed3d7
sojeonglee <sojeonglee0125@gmail.com>
2022-11-27 09:47:35 +0900

    solved : BOJ_1094

---
+++ src/week4/BOJ_1094.java
@@ -0,0 +1,17 @@
+package week4;
+
+import java.util.Scanner;
+
+public class BOJ_1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int cnt = 1;
+        while (X != 1) {
+            if ((X & 1) != 0) cnt++;
+            X /= 2;
+        }
+        System.out.println(cnt);
+
+    }
+}
