ad69d68cd5d5531515ce9c8bb669a1684f1e2dab
sojeonglee <sojeonglee0125@gmail.com>
2022-12-08 11:51:11 +0900

    solved : BOJ_Step11_2447 & BOJ_Step11_11729

--- src/baekjoonStep/BOJ_Step11_2447.java
+++ src/baekjoonStep/BOJ_Step11_2447.java
@@ -1,10 +1,10 @@
-package week3;
+package baekjoonStep;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_2447 {
+public class BOJ_Step11_2447 {
     static char[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
